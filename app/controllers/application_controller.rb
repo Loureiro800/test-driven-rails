238712e91e81020159334209c6adad5aa21189be
Pedro Loureiro <pedro@kudocs.co.uk>
2020-11-02 18:10:51 +0000

    minimal sign in added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def authenticate
+    redirect_to(new_session_path) if is_signed_in
+  end
+
+  private
+
+  def is_signed_in
+    session[:current_email].present?
+  end
 end
--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -1,7 +1,11 @@
 module Features
   module SessionHelpers
+
     def sign_in
-      visit root_path
+      visit new_session_path
+      fill_in 'Email', with: 'pedro@kudocs.co.uk'
+      click_on 'Sign in'
     end
+
   end
 end
