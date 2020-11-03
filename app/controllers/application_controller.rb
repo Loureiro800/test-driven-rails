782e1526def7923ee7b960a916e0f26ffa768ba8
Pedro Loureiro <pedro@kudocs.co.uk>
2020-11-03 11:07:39 +0000

    syntax update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
 
   def require_authenticated
-    redirect_to(new_session_path) if is_signed_in
+    redirect_to(new_session_path) if !is_signed_in
   end
 
   private
 
   def is_signed_in
-    session[:current_email].blank?
+    session[:current_email].present?
   end
 end
