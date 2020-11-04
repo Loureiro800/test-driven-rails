12e0d6ae3122f54c8619175352dd5e2a4dd10903
Pedro Loureiro <pedro@kudocs.co.uk>
2020-11-04 12:11:43 +0000

    created way to remove completed status (based on test-driven)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :todos, only: [:index, :new, :create] do
     get '/completions' => 'completions#create', as: :completion
+    get '/completions#destroy' => 'completions#destroy', as: :completion_destroy
   end
 
   resource :session, only: [:new, :create]
